428d29d875822126202fce473ef9b652eeb3e37a
koresachin7 <koresachin7@gmail.com>
2021-09-08 23:41:34 +0530

    Added : Roll Dice get Number to Use Random

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,8 +1,20 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+    int position =0; //initialize position
+
     public static void main(String[] args) {
-        int position = 0;  //initialize position
-        System.out.println("Welcome to Snake and ladder Problem");
+        SnakeAndLadder snakeLad = new SnakeAndLadder();
+        int diceValue = snakeLad.rollDice(); // call method her and store method return value
+        System.out.println(+diceValue);
+    }
+
+    public int rollDice() {
+        /* Method used Math class
+         * to Generating Random number between 1 to 6
+         */
+        int dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Dice Roll is : " + dice);
+        return dice;
     }
 }
