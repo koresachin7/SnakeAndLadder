2a13382b445a3bb898c6b88e9f21eb6e81be0493
koresachin7 <koresachin7@gmail.com>
2021-09-08 23:00:37 +0530

    Added : add Position in the program

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
+        int position = 1;  //initialize position
         System.out.println("Welcome to Snake and ladder Problem");
     }
 }
